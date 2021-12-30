dbadba26cbb461bdafac51ebad3f5bad11125885
keerthivasan <75238669+keerthivasanm20@users.noreply.github.com>
2021-12-30 18:45:25 +0530

    first commit

--- src/com/presidio/data/Driver.scala
+++ src/com/presidio/data/Driver.scala
@@ -1,15 +1,16 @@
 package com.presidio.data
 
-import com.presidio.data.reader.{CSVReader, Reader}
+import com.presidio.data.reader.{CSVReader, Reader, RevenueCalculator}
 import org.apache.spark.sql.SparkSession
 
 object Driver {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession builder () appName "SalesDataProcessingJob" master "spark://giridhar:7077" getOrCreate ()
+    val spark = SparkSession builder () appName "SalesDataProcessingJob" master "spark://keerthivasan-VirtualBox:7077" getOrCreate ()
 
     val reader: Reader = new CSVReader
-    reader readAsDF (spark, "/home/giridhar/test/sales.csv") show 10
-
+    val FileDf=reader readAsDF(spark, "/home/keerthivasan/Downloads/sales.csv")
+    val CalculatedDf=new RevenueCalculator calculateTotalRevenue(FileDf)
+    CalculatedDf.show()
     spark close
   }
 }
