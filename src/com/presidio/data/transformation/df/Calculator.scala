d6bc5691662d075208ff9c88497b43eb00c9d146
Sai Rakesh <sai.rakesh820@gmail.com>
2022-01-02 12:26:49 +0530

    updated revenue calculator

--- src/com/presidio/data/transformation/df/Calculator.scala
+++ src/com/presidio/data/transformation/df/Calculator.scala
@@ -2,7 +2,7 @@ package com.presidio.data.transformation.df
 
 import org.apache.spark.sql.DataFrame
 
-//
+// This trait calculator is used to calculating the total revenue in the provided dataframe
 trait Calculator {
   def calculateTotalRevenue(fileDf: DataFrame): DataFrame
 }
